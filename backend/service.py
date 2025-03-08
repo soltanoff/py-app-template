946fcc4b4394fb16ff2a6b05ce95544c97d1267e
soltanoff <ilia@soltanov.dev>
2025-03-08 21:13:28 +0300

    Let's go, CI!

--- backend/service.py
+++ backend/service.py
@@ -1,6 +1,13 @@
+import hashlib
+
+
 def root() -> dict[str, str]:
     return {"message": "Hello World"}
 
 
 def say_hello(name: str) -> dict[str, str]:
     return {"message": f"Hello {name}"}
+
+
+def say_goodbye(name: str) -> dict[str, str]:
+    return {"message": hashlib.md5(name.encode()).hexdigest()}
